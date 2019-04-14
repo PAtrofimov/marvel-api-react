3ec204a5a3025ddb553906b454e0857fdfcc051a
Peter Trofimov <ptrofimov7@gmail.com>
2019-04-14 22:29:12 +0300

    Updates

---
+++ precache-manifest.2ec8c2251942ed97b7e2f1482c5fae3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f78169b64594c1a8806",
+    "url": "/marvel-api-react/static/js/runtime~main.7a613dc0.js"
+  },
+  {
+    "revision": "c5ed765a100373405f32",
+    "url": "/marvel-api-react/static/js/main.ddf16931.chunk.js"
+  },
+  {
+    "revision": "dff33177d1690868b1cd",
+    "url": "/marvel-api-react/static/js/2.f6698d55.chunk.js"
+  },
+  {
+    "revision": "c5ed765a100373405f32",
+    "url": "/marvel-api-react/static/css/main.92bad4c7.chunk.css"
+  },
+  {
+    "revision": "9a61e48793dbf264fe145efa2306fb03",
+    "url": "/marvel-api-react/index.html"
+  }
+];
